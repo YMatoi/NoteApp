cefdd514ba74c2efda432f41cbcb4df09aefee82
YMatoi <YMatoi@users.noreply.github.com>
2020-01-04 21:49:12 +0900

    use strings.xml

--- app/src/main/java/com/github/ymatoi/note/ui/DeleteConfirmDialog.kt
+++ app/src/main/java/com/github/ymatoi/note/ui/DeleteConfirmDialog.kt
@@ -5,6 +5,7 @@ import android.app.Dialog
 import android.content.DialogInterface
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
+import com.github.ymatoi.note.R
 
 class DeleteConfirmDialog : DialogFragment() {
     companion object {
@@ -16,9 +17,9 @@ class DeleteConfirmDialog : DialogFragment() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return AlertDialog.Builder(context)
-            .setMessage("削除しますか")
-            .setPositiveButton("削除", onPositiveButtonListener)
-            .setNegativeButton("キャンセル") { _, _ ->
+            .setMessage(R.string.confirm_delete_dialog_message)
+            .setPositiveButton(R.string.delete, onPositiveButtonListener)
+            .setNegativeButton(R.string.cancel) { _, _ ->
                 dismiss()
             }
             .create()
