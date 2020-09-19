82f5d36917df11fab3e74e3daca6a62f15d787fb
YMatoi <YMatoi@users.noreply.github.com>
2020-09-20 05:01:03 +0900

    remove unused functions

--- app/src/main/java/com/github/ymatoi/note/util/CalendarExt.kt
+++ app/src/main/java/com/github/ymatoi/note/util/CalendarExt.kt
@@ -1,33 +1,8 @@
 package com.github.ymatoi.note.util
 
 import android.text.format.DateFormat
-import java.text.SimpleDateFormat
 import java.util.Calendar
-import java.util.Locale
-
-fun Calendar.setZeroTime() {
-    set(Calendar.HOUR, 0)
-    set(Calendar.MINUTE, 0)
-    set(Calendar.SECOND, 0)
-}
-
-fun Calendar.setNextDate() {
-    add(Calendar.DATE, 1)
-}
-
-fun Calendar.setPrevDate() {
-    add(Calendar.DATE, -1)
-}
-
-fun Calendar.copy() = this.clone() as Calendar
 
 fun Calendar.dateText() = DateFormat.format("yyyy/MM/dd (E)", this).toString()
-fun Calendar.parseFromDateText(text: String): Calendar {
-    runCatching {
-        time = SimpleDateFormat("yyyy/MM/dd (E)", Locale.JAPAN).parse(text)!!
-    }.onFailure {
-    }
-    return this
-}
 fun Calendar.dateTimeText() = DateFormat.format("yyyy/MM/dd (E) kk:mm", this).toString()
 fun Calendar.timeText() = DateFormat.format("kk:mm", this).toString()
