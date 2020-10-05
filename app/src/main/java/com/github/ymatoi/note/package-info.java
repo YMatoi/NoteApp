b6dd9bd46d32d3e8895319718b7397fe7a4b7631
YMatoi <YMatoi@users.noreply.github.com>
2020-10-05 23:16:48 +0900

    upgrade dependencies

--- app/src/main/java/com/github/ymatoi/note/package-info.java
+++ app/src/main/java/com/github/ymatoi/note/package-info.java
@@ -1,4 +1,5 @@
-@EpoxyDataBindingPattern(rClass = R.class, layoutPrefix = "epoxy")
 package com.github.ymatoi.note;
 
 import com.airbnb.epoxy.EpoxyDataBindingPattern;
+@EpoxyDataBindingPattern(rClass = R.class, layoutPrefix = "epoxy")
+interface EpoxyDataBindingConfig {}
