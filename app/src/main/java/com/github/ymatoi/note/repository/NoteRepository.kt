32953b989bf48365dddd6f8a41199f02899cbc71
YMatoi <YMatoi@users.noreply.github.com>
2020-10-26 22:46:52 +0900

    divide firestore dao

--- app/src/main/java/com/github/ymatoi/note/repository/NoteRepository.kt
+++ app/src/main/java/com/github/ymatoi/note/repository/NoteRepository.kt
@@ -1,64 +1,44 @@
 package com.github.ymatoi.note.repository
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import com.github.ymatoi.note.database.Note
 import com.github.ymatoi.note.database.NoteDatabase
-import com.github.ymatoi.note.util.combine
+import com.github.ymatoi.note.firestore.NoteFirestoreDao
 import com.google.firebase.auth.ktx.auth
-import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import javax.inject.Inject
 
 class NoteRepository @Inject constructor(
-    private val database: NoteDatabase
+    private val database: NoteDatabase,
+    private val firestore: NoteFirestoreDao
 ) {
     private val currentUser get() = Firebase.auth.currentUser
-    private val collection get() = currentUser?.let { FirebaseFirestore.getInstance().collection("/users/${it.uid}/notes") }
 
-    private val _notes = MutableLiveData<List<Note>>()
-    private fun getAllFromFirestore(): LiveData<List<Note>> = _notes.also {
-        collection?.get()?.addOnSuccessListener {
-            val notes = it.toObjects(Note::class.java).sortedBy { it.recordedAt }.reversed()
-            _notes.postValue(notes)
-        }
-    }
     fun getAll() = when (currentUser == null) {
         true -> database.noteDao().getAll()
-        false -> getAllFromFirestore()
+        false -> firestore.getAll()
     }
 
-    private val _query = MutableLiveData<String>()
-    private val _searchedText = combine(_query, _notes) { query, notes ->
-        notes?.filter { when (query) {
-            null -> true
-            else -> it.text.contains(Regex.fromLiteral(query))
-        } }?.sortedBy { it.recordedAt }?.reversed()
-    }
-    private fun findByTextFirestore(query: String) = _searchedText.also {
-        _query.postValue(query)
-    }
     fun findByText(query: String) = when (currentUser == null) {
         true -> database.noteDao().findByText(query)
-        false -> findByTextFirestore(query)
+        false -> firestore.findByText(query)
     }
 
     suspend fun insert(note: Note) {
         when (currentUser == null) {
             true -> database.noteDao().insert(note)
-            false -> collection?.document(note.uuid)?.set(note)
+            false -> firestore.insert(note)
         }
     }
     suspend fun update(note: Note) {
         when (currentUser == null) {
             true -> database.noteDao().update(note)
-            false -> collection?.document(note.uuid)?.set(note)
+            false -> firestore.update(note)
         }
     }
     suspend fun delete(note: Note) {
         when (currentUser == null) {
             true -> database.noteDao().delete(note)
-            false -> collection?.document(note.uuid)?.delete()
+            false -> firestore.delete(note)
         }
     }
 }
